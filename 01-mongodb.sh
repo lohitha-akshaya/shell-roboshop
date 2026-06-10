a1aa16a6b89e5ba43b6842a808381c57a40d089a
lohitha-akshaya <lohithaakshaya18@gmail.com>
2026-06-10 19:25:55 +0530

    mongodb

--- 01-mongodb.sh
+++ 01-mongodb.sh
@@ -38,8 +38,17 @@ VALIDATE  $? "Adding Mongo repo"
 dnf install mongodb-org -y  &>> $LOG_FILE 
 VALIDATE $? "Installing Mongodb"
 
+#starting and enabling mongodb
 systemctl enable --now mongod
 VALIDATE $? "Starting  and enabling mongodb"
+
+#changing bind IP 
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connection to mongodb"
+
+#restarting mongodb
+systemctl restart mongod
+VALIDATE $? "Restarting mongodb"
 
 
    
