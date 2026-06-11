5ca073c98e2f46058e93d565294f7a44926c8aac
lohitha-akshaya <lohithaakshaya18@gmail.com>
2026-06-11 16:29:15 +0530

    mongodb

--- roboserver.sh
+++ roboserver.sh
@@ -10,7 +10,7 @@ do
     INSTANCE_ID=$(aws ec2 run-instances  \
         --image-id $AMI_ID \
         --instance-type t3.micro \
-        --security-group-ids sg-04f06b5011da09b6d \
+        --security-groups roboshop-common "roboshop-$instance"  \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="$instance"}]" \
         --query "Instances[0].InstanceId" \
         --output text)
