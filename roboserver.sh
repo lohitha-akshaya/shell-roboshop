94e01afc0701e80f1abe2338847a89b5c466aa68
lohitha-akshaya <lohithaakshaya18@gmail.com>
2026-06-11 16:13:29 +0530

    mongodb

--- roboserver.sh
+++ roboserver.sh
@@ -1,59 +1,19 @@
-#!/bin/bash
+#!/bin/bash 
+
 AMI_ID="ami-0220d79f3f480ecf5"
-ZONE_ID="Z0653444S9M5BRWXUG0B" # hosted zone id 
-DOMAIN_NAME="lohithadev.online" #domain name
+ZONE_ID="Z0653444S9M5BRWXUG0B"
+DOMAIN_NAME="lohithadev.online"
 
 for instance in $@
-do
-    echo "Launching instance: $instance"
-    INSTANCE_ID=$(aws ec2 run-instances \
-        --image-id ami-0220d79f3f480ecf5 \
+do 
+    echo "Launching instance:$instance"
+    INSTANCE_ID=$(aws ec2 rub-instances  \
+        --image-id $AMI_ID \
         --instance-type t3.micro \
-        --security-groups "roboshop-common" "roboshop-$instance" \
-         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="roboshop-$instance}]" \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
-        --query 'Instances[0].InstanceId' \
-        --output text
-    )
-    echo "Instance ID: $INSTANCE_ID"
+        --security-groups "roboshop-commmon" "roboshop-$instance"  \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+        --query "Instances[0].InstanceId" \
+        --output text)
 
-    if [ $instance == "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
-         --query 'Reservations[*].Instances[*].PublicIpAddress' \
-         --output text
-        )
-        R53_RECORD="$DOMAIN_NAME"
-    else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
-         --query 'Reservations[*].Instances[*].PrivateIpAddress' \
-         --output text
-        )
-        R53_RECORD="$instance.$DOMAIN_NAME"
-    fi
-
-    #### Updating R53 Record ####
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-        {
-            "Comment": "Update A record to new IP",
-            "Changes": [
-                {
-                    "Action": "UPSERT",
-                    "ResourceRecordSet": {
-                        "Name": "'$R53_RECORD'",
-                        "Type": "A",
-                        "TTL": 1,
-                        "ResourceRecords": [
-                            {
-                                "Value": "'$IP'"
-                            }
-                        ]
-                    }
-                }
-            ]
-        }
-
-    '
-
-done        
+    echo "Instance ID : $INSTANCE_ID"
+done
