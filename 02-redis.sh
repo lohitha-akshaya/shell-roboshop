0d8edd152f6370688e75a622458481b9496f2ddc
lohitha-akshaya <lohithaakshaya18@gmail.com>
2026-06-11 18:28:34 +0530

    mongodb

--- 02-redis.sh
+++ 02-redis.sh
@@ -30,16 +30,16 @@ VALIDATE() {
 
 }
 #installing redis
-dnf module disable redis -y
-dnf module enable redis:7 -y
-dnf install redis -y
+dnf module disable redis -y  &>> $LOG_FILE
+dnf module enable redis:7 -y  &>> $LOG_FILE
+dnf install redis -y   &>> $LOG_FILE
 VALIDATE $? "Installing redis"
 
 # changing bind IP and protected mode
-sed -t -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/c protected-mode no' -i /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/c protected-mode no' -i /etc/redis/redis.conf
 VALIDATE $? "Allowing remote access "
 
 #starting and enabling redis
-systemctl enable redis
-systemctl start redis
+systemctl enable redis &>> $LOG_FILE
+systemctl start redis  &>> $LOG_FILE
 VALIDATE $? "Starting and enabling redis"
