abb46d0b4309c9e13f51e0a26255a97e915835c6
lohitha-akshaya <lohithaakshaya18@gmail.com>
2026-06-11 17:59:34 +0530

    mongodb

--- roboserver.sh
+++ roboserver.sh
@@ -16,4 +16,47 @@ do
         --output text)
 
     echo "Instance ID : $INSTANCE_ID"
+
+    if [ $instance == "frontend" ]; then
+     IP=$(aws ec2 describe-instances \
+        --instance-ids $INSTANCE_ID \
+        --query "Reservations[*].Instances[*].PublicIpAddress" \
+        --output text
+     )
+     R53_RECORD="$DOMAIAN_NAME"
+    else
+       IP=$(aws ec2 describe-instances \
+        --instance-ids $INSTANCE_ID \
+        --query "Reservations[*].Instances[*].PrivateIpAddress" \
+        --output text
+       )
+       R53_RECORD="$instance.$DOMAIN_NAME"
+    fi
+
+    #  updating R53 Record 
+     aws route53 change-resource-record-sets \
+        --hosted-zone-id $ZONE_ID \
+        --change-batch '
+                {
+                   "Comment": "Updating a record to new IP",
+                    "Changes": [
+                        {
+
+                           "Action": "UPSERT",
+                           "ResourceRecordSet": {
+                                "Name": "'$R53_RECORD'",
+                                "Type": "A",
+                                "TTL": 1,
+                                "ResourceRecords": [
+                            {
+                                "Value": "'$IP'"
+                            }
+                        ]
+                    }
+                }
+            ]
+        
+        }   
+        '
 done
+ 
