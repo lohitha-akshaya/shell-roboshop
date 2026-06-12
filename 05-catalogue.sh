3ff69ca16edbb7e6f3c72b0eb5aad450d2d22529
lohitha-akshaya <lohithaakshaya18@gmail.com>
2026-06-12 14:16:52 +0530

    roboshop

--- 05-catalogue.sh
+++ 05-catalogue.sh
@@ -67,7 +67,7 @@ VALIDATE $? "Added Mongo repo"
 dnf install mongodb-mongosh -y &>>$LOGS_FILE
 VALIDATE $? "Installed MongoDB client"
 
-INDEX=$(mongosh --host mongodb.lohithadev.online --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+INDEX=$(mongosh --host mongodb.lohithadev.online --eval 'db.getMongo().getDBNames().indexOf("catalogue"))
 
 if [ $INDEX -lt 0 ]; then
     mongosh --host mongodb.lohithadev.online </app/db/master-data.js &>>$LOGS_FILE
