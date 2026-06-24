1260b3c7584c33b1a61c8f10488c60a5bc43a0c6
lohitha-akshaya <lohithaakshaya18@gmail.com>
2026-06-24 22:30:31 +0530

    ansible

--- state.sh
+++ state.sh
@@ -1,5 +1,7 @@
-state:$(aws ec2 describe-instances \
+
+
+state=$(aws ec2 describe-instances \
     --query "Reservations[*].Instances[*].State.Name" \
-    --output table
-)
+    --output text)
+
 echo "$state"
