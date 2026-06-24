b353cf97de792117b6a8388a2114ad4278249efa
lohitha-akshaya <lohithaakshaya18@gmail.com>
2026-06-24 22:41:26 +0530

    ansible

--- state.sh
+++ state.sh
@@ -1,12 +1,20 @@
+aws ec2 describe-instances \
+    --query "Reservations[*].Instances[*].[InstanceId, State.Name]" \
+    --output text | while read -r instance_id state; do
+        
+        if [ "$state" = "stopped" ]; then
+            echo "$instance_id is stopped"
+            
+        elif [ "$state" = "running" ]; then
+            echo "$instance_id is running safely"
+            
+        elif [ "$state" = "terminated" ]; then
+            echo "$instance_id has been terminated"
+            
+        else
+            echo "$instance_id is in an unknown state: $state"
+        fi
 
-
-state=$(aws ec2 describe-instances \
-    --query "Reservations[*].Instances[*].[InstanceId,State.Name]" \
-    --output text)
-
-echo "$state"
-  if [ "$state" = "stopped" ]; then
-    echo "$instance_id is stopped"
-  fi
+done
 
 
